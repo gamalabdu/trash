5d1dc260facb8bb435b093df616ec0809142a9cc
Gamal Abdu <abdu.s.gamal@gmail.com>
2023-07-17 23:18:14 -0400

    new changes idk

--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -4,49 +4,40 @@ import './styles.css'
 import GalleryItem from './GalleryItem/GalleryItem'
 import { Link } from 'react-router-dom'
 
-
-
 interface IGalleryProps {
-    categories: string[] 
-    data: {
-        image: string
-        type: string[]
-        name: string
-        videos: string[]
-        images? : string[]
-        iphone: boolean
-        assets? : string[]
-        canvas?: string[]
-    }[]
+	categories: string[]
+	data: {
+		image: string
+		type: string[]
+		name: string
+		videos: string[]
+		images?: string[]
+		iphone: boolean
+		assets?: string[]
+		canvas?: string[]
+	}[]
 }
 
-const Gallery = (props : IGalleryProps) => {
+const Gallery = (props: IGalleryProps) => {
+	const { categories, data } = props
 
+	const [galleryData, setGalleryData] = useState(data)
 
-  const { categories, data } = props
+	useEffect(() => {
+		setGalleryData(data)
+	}, [data])
 
-  const [galleryData, setGalleryData ] = useState(data)
-
-  useEffect(() => {
-    setGalleryData(data);
-  }, [data]);
-
-  return (
-    <motion.div
-    layout
-    className='gallery-container'>
-        <AnimatePresence>
-        {
-          galleryData.map((item, i) => (
-
-            <Link key={i} to='/innerworks' state={{ item }} >
-              <GalleryItem item={item} />
-            </Link>
-
-        ))}
-        </AnimatePresence>
-    </motion.div>
-  )
+	return (
+		<motion.div layout className='gallery-container'>
+			<AnimatePresence>
+				{galleryData.map((item, i) => (
+					<Link key={i} to='/innerworks' state={{ item }}>
+						<GalleryItem item={item} />
+					</Link>
+				))}
+			</AnimatePresence>
+		</motion.div>
+	)
 }
 
 export default Gallery
