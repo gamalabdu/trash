270d94eced3bee5ba014748201928d8f07cb1b9e
Gamal Abdu <abdu.s.gamal@gmail.com>
2023-07-25 23:21:34 -0400

    some more changes jesus

--- src/components/WorksGallery/WorksGallery.tsx
+++ src/components/WorksGallery/WorksGallery.tsx
@@ -1,51 +1,5 @@
-// import React, { useEffect, useState } from 'react'
-// import { CSSTransition, SwitchTransition } from 'react-transition-group'
-// import './styles.css'
-
-// interface WorksGalleryProps {
-// 	data: {
-// 		image: any
-// 		type: string
-// 	}[]
-// 	categories: string[]
-// }
-
-// const WorksGallery = (props: WorksGalleryProps) => {
-    
-// 	const { data, categories } = props
-
-// 	return (
-// 		<div className='works-gallery-container'>
-// 			{
-                
-//                 data.map((item, i) => {
-				
-//                 return (
-// 					// <div key={i} className={`image-container ${state.status}`}>
-					
-// 								<div key={i} className={'image-container'}>
-// 									<img className='image' src={item.image} alt='pic' />
-// 								</div>
-			
-
-// 					// <div key={i} className='image-container'>
-// 					// 	<img className='image'  src={item.image} alt='pic' />
-// 					// </div>
-// 				)
-// 			})}
-// 		</div>
-// 	)
-// }
-
-// export default WorksGallery
-
-
-
-
-
 
 import React, { useEffect, useState } from 'react';
-import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import './styles.css';
 
 interface WorksGalleryProps {
@@ -67,16 +21,15 @@ const WorksGallery = (props: WorksGalleryProps) => {
 
   return (
     <div className='works-gallery-container'>
-      <TransitionGroup>
+
         {
           galleryData.map((item, i) => (
 
-          <CSSTransition key={i} classNames='fade' timeout={400}>
+
               <img  className='image' src={item.image} alt='pic' />
-          </CSSTransition>
+
 
         ))}
-      </TransitionGroup>
     </div>
   );
 };
