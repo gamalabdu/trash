965b4c112b100bf38282a0c3661208b7f678bf2c
Gamal Abdu <abdu.s.gamal@gmail.com>
2023-07-27 17:17:42 -0400

    strict mode

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,14 @@
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-// Type for the root variable
-const root: ReactDOM.Root = ReactDOM.createRoot(
+const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-
-root.render(<App />);
-
-reportWebVitals();
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
