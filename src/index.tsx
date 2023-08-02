04442943b4de3da58a2af0d07dbe6f5f1485406e
Gamal Abdu <abdu.s.gamal@gmail.com>
2023-08-02 00:09:34 -0400

    more trying

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,26 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
+
+// const root = ReactDOM.createRoot(
+//   document!.getElementById('root') as HTMLElement
+// )
+
+// root.render(
+//     <App />
+// );
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(
-  document!.getElementById('root') as HTMLElement
-)
+const rootElement = document.getElementById('root');
 
-root.render(
-    <App />
-);
+if (rootElement instanceof HTMLElement) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(<App />);
+} else {
+  console.error('Unable to find root element with id "root"');
+}
