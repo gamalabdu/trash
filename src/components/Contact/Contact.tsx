a03946feba984acdf136c72b0707c40cc05638f3
Gamal Abdu <abdu.s.gamal@gmail.com>
2023-08-01 22:59:59 -0400

    trying to clean up event lisenters on sidenav

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -59,10 +59,10 @@ const Contact = () => {
 		}
 	}
 
-	const isValidEmail = (email: string) => {
-		const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-		return regex.test(String(email).toLowerCase());
-	};
+	// const isValidEmail = (email: string) => {
+	// 	const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	// 	return regex.test(String(email).toLowerCase());
+	// };
     
 
 	return (
@@ -116,7 +116,7 @@ const Contact = () => {
 											className='app-form-control' 
 											placeholder='EMAIL' 
 											value={email}
-											onInput={ (e) => isValidEmail(e.currentTarget.value) ?  setEmail( e.currentTarget.value) : setEmail('Not a valid Email')}
+											onInput={ (e) => setEmail( e.currentTarget.value) }
 										/>
 									</div>
 									<div className='app-form-group'>
