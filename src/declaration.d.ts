fc34badc6e4fb5d3ea471aa07dc4f3c697a09d37
Gamal Abdu <abdu.s.gamal@gmail.com>
2023-08-02 16:40:00 -0400

    clean up some more

--- src/declaration.d.ts
+++ src/declaration.d.ts
@@ -1,10 +1,4 @@
-declare module '*.mp4' {
-    const content: any;
-    export default content;
-  }
-
-  // declare module 'react';
-
+  declare module '*.mp4';
   declare module '*.png';
   declare module '*.jpeg';
   declare module '*.jpg';
