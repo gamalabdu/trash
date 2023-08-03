462093453954613dec303c445b69a7d85712e159
Gamal Abdu <abdu.s.gamal@gmail.com>
2023-08-02 22:30:21 -0400

    more changes again

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -26,7 +26,7 @@ const Footer = () => {
 
 
   return (
-    <footer className='footer-container'>
+    <div className='footer-container'>
 
         <div className='footer-black'>
 
@@ -66,7 +66,7 @@ const Footer = () => {
 
         </div>
 
-    </footer>
+    </div>
   )
 }
 
--- src/components/NavContainer/NavContainer.tsx
+++ src/components/NavContainer/NavContainer.tsx
@@ -1,67 +1,133 @@
-import React, { useEffect, useState } from 'react'
-import { Link, Outlet } from 'react-router-dom'
-import SideNav from '../SideNav/SideNav'
-import Footer from '../Footer/Footer'
-import { FiArrowDown } from 'react-icons/fi'
-import { IoChatboxOutline } from 'react-icons/io5'
-import './styles.css'
+import React, { useEffect, useState } from 'react';
+import { Link, Outlet } from 'react-router-dom';
+import SideNav from '../SideNav/SideNav';
+import Footer from '../Footer/Footer';
+import { FiArrowDown } from 'react-icons/fi';
+import { IoChatboxOutline } from 'react-icons/io5';
+import './styles.css';
 
 const NavContainer = () => {
 
-	const [isVisible, setIsVisible] = useState(false)
+  const [isVisible, setIsVisible] = useState(false);
 
-	// Top: 0 takes us all the way back to the top of the page
-	// Behavior: smooth keeps it smooth!
+  const scrollToBottom = () => {
 
-	const scrollToBottom = () => {
-		const scrollHeight = document.documentElement?.scrollHeight || 0;
+    const scrollHeight = document.documentElement.scrollHeight || 0;
+
+    if (scrollHeight > 0) {
+      window.scrollTo({
+        top: scrollHeight,
+        behavior: 'smooth',
+      });
+    }
+  }
+
+
+  useEffect(() => {
+    const toggleVisibility = () => {
+      const scrollHeight = document.documentElement.scrollHeight || 0;
+      const pageYOffset = window.pageYOffset || 0;
+      const innerHeight = window.innerHeight || 0;
+
+      setIsVisible(pageYOffset > 50 && !(pageYOffset + 50 >= scrollHeight + 10 - innerHeight + 10));
+    };
+
+    window.addEventListener('scroll', toggleVisibility);
+
+    return () => {
+      window.removeEventListener('scroll', toggleVisibility);
+    };
+  }, []);
+
+  return (
+    <div>
+      <SideNav />
+      <Outlet />
+      <div className={`scroll-to-top ${isVisible ? '' : 'scroll-to-top-active'}`}>
+        <FiArrowDown size={20} onClick={scrollToBottom} id='scrollToTopBtn' />
+        <Link to='/contact' style={{ textDecoration: 'none' }}>
+          <IoChatboxOutline size={20} />
+        </Link>
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default NavContainer;
+
+
+
+
+
+
+
+
+
+// import React, { useEffect, useState } from 'react'
+// import { Link, Outlet } from 'react-router-dom'
+// import SideNav from '../SideNav/SideNav'
+// import Footer from '../Footer/Footer'
+// import { FiArrowDown } from 'react-icons/fi'
+// import { IoChatboxOutline } from 'react-icons/io5'
+// import './styles.css'
+
+// const NavContainer = () => {
+
+// 	const [isVisible, setIsVisible] = useState(false)
+
+// 	// Top: 0 takes us all the way back to the top of the page
+// 	// Behavior: smooth keeps it smooth!
+
+// 	const scrollToBottom = () => {
+// 		const scrollHeight = document.documentElement?.scrollHeight || 0;
 	
-		if (scrollHeight > 0) {
-			window.scrollTo({
-				top: scrollHeight,
-				behavior: 'smooth',
-			});
-		}
-	};
+// 		if (scrollHeight > 0) {
+// 			window.scrollTo({
+// 				top: scrollHeight,
+// 				behavior: 'smooth',
+// 			});
+// 		}
+// 	};
 
 
 
-	useEffect(() => {
-		const toggleVisibility = () => {
-			const scrollHeight = document?.documentElement?.scrollHeight || 0;
-			const pageYOffset = window?.pageYOffset || 0;
-			const innerHeight = window?.innerHeight || 0;
+// 	useEffect(() => {
+// 		const toggleVisibility = () => {
+// 			const scrollHeight = document?.documentElement?.scrollHeight || 0;
+// 			const pageYOffset = window?.pageYOffset || 0;
+// 			const innerHeight = window?.innerHeight || 0;
 			
-			if (pageYOffset > 50 && !(pageYOffset + 50 >= scrollHeight + 10 - innerHeight + 10)) {
-				setIsVisible(true);
-			} else {
-				setIsVisible(false);
-			}
-		};
+// 			if (pageYOffset > 50 && !(pageYOffset + 50 >= scrollHeight + 10 - innerHeight + 10)) {
+// 				setIsVisible(true);
+// 			} else {
+// 				setIsVisible(false);
+// 			}
+// 		};
 	
-		window?.addEventListener('scroll', toggleVisibility);
+// 		window?.addEventListener('scroll', toggleVisibility);
 	
-		return () => {
-			window?.removeEventListener('scroll', toggleVisibility);
-		};
-	}, []);
+// 		return () => {
+// 			window?.removeEventListener('scroll', toggleVisibility);
+// 		};
+// 	}, []);
 	
 
-	return (
-		<div>
-			<SideNav />
-			<Outlet />
-				<div className={ isVisible ? 'scroll-to-top' : 'scroll-to-top-active'}>
-					<FiArrowDown size={20} 
-					onClick={() => scrollToBottom() } 
-					id='scrollToTopBtn' />
-					<Link to='/contact' style={{textDecoration:"none"}}>
-					  <IoChatboxOutline size={20} />
-				    </Link>
-				</div>
-			<Footer />
-		</div>
-	)
-}
+// 	return (
+// 		<div>
+// 			<SideNav />
+// 			<Outlet />
+// 				<div className={ isVisible ? 'scroll-to-top' : 'scroll-to-top-active'}>
+// 					<FiArrowDown size={20} 
+// 					onClick={() => scrollToBottom() } 
+// 					id='scrollToTopBtn' />
+// 					<Link to='/contact' style={{textDecoration:"none"}}>
+// 					  <IoChatboxOutline size={20} />
+// 				    </Link>
+// 				</div>
+// 			<Footer />
+// 		</div>
+// 	)
+// }
 
-export default NavContainer
+// export default NavContainer
