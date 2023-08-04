b03075f35201dbbecaadf4fe6f51038fb36da02e
Gamal Abdu <abdu.s.gamal@gmail.com>
2023-08-04 19:48:39 -0400

    wtf

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-const rootElement = document.getElementById('root')
-
-if (rootElement instanceof HTMLElement) {
-  const root = ReactDOM.createRoot(rootElement)
-  root.render(
-      <App />
-  )
-} else {
-  console.error('Unable to find root element with id "root"')
-}
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
