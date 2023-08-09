5f2e3b9a47e784cffb09281265554a8df6f6df16
Gamal Abdu <abdu.s.gamal@gmail.com>
2023-08-08 22:29:06 -0400

    more changes

--- src/App.tsx
+++ src/App.tsx
@@ -23,13 +23,13 @@ function App() {
 
               <Route element={ <NavContainer /> }>
 
-                  <Route path="/home" element={ <Home /> } />
+                  {/* <Route path="/home" element={ <Home /> } />
                   <Route path="/about" element={ <About /> } />
                   <Route path="/for-artist" element={ <ForArtist /> } />
                   <Route path="/for-clients" element={ <ForClients /> } />
                   <Route path="/works" element={ <Works /> } />
                   <Route path='/innerworks' element={ <InnerWork /> } />
-                  <Route path="/contact" element={ <Contact /> } />
+                  <Route path="/contact" element={ <Contact /> } /> */}
 
               </Route>
 
